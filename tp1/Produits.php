bcac6caffeb45cbd3a53ef93c9f1b555c388964c
Khaldi Mohamed <moh78078000@gmail.com>
2025-02-08 12:17:43 +0100

    "Amélioration des méthodes + création d'un fichier csv incluant tva ue et non ue pour 100 items"

--- tp1/Produits.php
+++ tp1/Produits.php
@@ -1,62 +1,29 @@
 <?php
-
-
 class Produits {
     public $nom;
-
     public $origin;
-
     public $prix_HT;
-
     public $prix_TTC;
-
     public $tva_UE = 1.15;
-
     public $tva_hors_UE = 1.18;
 
-    public function __construct($nom, $origin , $prix_HT , $prix_TTC) {
+    public function __construct($nom, $origin, $prix_HT) {
         $this->nom = $nom;
         $this->origin = $origin;
-        $this-> prix_HT = $prix_HT;
-        $this-> prix_TTC = $prix_TTC;
-
+        $this->prix_HT = (float) $prix_HT;
+        $this->calculTVA();
     }
 
-
-    public function information(){
-        return("$this->nom , $this->origin , $this->prix_HT");
-    }
-
-
-    public function getprix_HT()
-    {
-        return $this->prix_HT;
-    }/**
- * @param mixed prix
- */
-
-
-
-    public function getOrigine(){
-        return $this->origin;
-    }
-
-    public function calculTVA(){
-        if ($this->origin == "UE"){
-            ($this->prixTTC = $this->prix_HT * $this->tva_UE)*100;
+    public function calculTVA() {
+        if ($this->origin == "UE") {
+            $this->prix_TTC = $this->prix_HT * $this->tva_UE;
+        } else {
+            $this->prix_TTC = $this->prix_HT * $this->tva_hors_UE;
         }
-
-        else {
-            ($this->prixTTC = $this->prix_HT * $this->tva_hors_UE)*100;
-
-        }
-
-
     }
 
-
-
-
-
-
+    public function getTVA() {
+        return $this->prix_TTC - $this->prix_HT;
+    }
 }
+?>
--- tp1/readcsv.php
+++ tp1/readcsv.php
@@ -1,34 +1,60 @@
 <?php
 
+require_once "Produits.php";
+
 echo "<h1> Produits </h1>";
 echo "<style>
-
-* {
-font-family: sans-serif;
-}
-
-
-table , td ,tr {
-border: 1px solid black;
-border-collapse: collapse;
-padding: 5px;
-}
+* { font-family: sans-serif; }
+table, td, tr { border: 1px solid black; border-collapse: collapse; padding: 5px; }
 </style>";
 
-
-
 $file = "produits.csv";
-$fp = fopen($file , "r");
+$file_output = "resultats_tva.csv";
+
+// Ouvrir les fichiers en lecture et écriture
+$fp = fopen($file, "r");
+$output = fopen($file_output, "w");
+
+// Écrire les en-têtes dans le fichier CSV
+fputcsv($output, ["Nom", "Origine", "Prix HT", "TVA sur 100 unités", "Prix TTC total (100 unités)"]);
+
 echo "<table>";
-while ($res=fgetcsv($fp , 1024 , ",")) {
-    echo "<tr>";
-    foreach ($res as $val ) {
-        echo "<td>.$val</td>";
-    }
-    echo "</tr>";
+
+// Lire ligne par ligne
+while ($res = fgetcsv($fp, 1024, ",")) {
+    // Ignorer l'en-tête du fichier CSV
+    if ($res[0] == "Nom") continue;
+
+    // Création de l'objet Produit
+    $produit = new Produits($res[0], $res[1], $res[2]);
+
+    // Calcul TVA sur 100 unités
+    $tva_items = $produit->getTVA() * 100;
+    $prix_ttc_total = $produit->prix_TTC * 100;
+
+    // Affichage HTML
+    echo "<tr>
+            <td>{$produit->nom}</td>
+            <td>{$produit->origin}</td>
+            <td>" . number_format($produit->prix_HT, 2, ',', ' ') . " €</td>
+            <td>" . number_format($tva_items, 2, ',', ' ') . " €</td>
+            <td>" . number_format($prix_ttc_total, 2, ',', ' ') . " €</td>
+          </tr>";
+
+    // Écriture dans le fichier CSV
+    fputcsv($output, [
+        $produit->nom,
+        $produit->origin,
+        number_format($produit->prix_HT, 2, ',', ' ') . " €",
+        number_format($tva_items, 2, ',', ' ') . " €",
+        number_format($prix_ttc_total, 2, ',', ' ') . " €"
+    ]);
 }
+
 echo "</table>";
 
-$produit = new Produits(res[0] , res[1] , res[2] );
+// Fermer les fichiers
+fclose($fp);
+fclose($output);
 
-
+?>
